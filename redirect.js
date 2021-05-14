eadeefb30bc204a3a827f354e12512e39d7c36d6
GamesThings <74464331+gamesthings@users.noreply.github.com>
2021-05-14 15:24:53 -0400

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+   <body>
+      <script>
+         setTimeout(function(){
+            window.location.href = 'https://www.sspcschool.us/home#fromgithub';
+         }, 5000);
+      </script>
+      <p>Redirecting you in 5 seconds.</p>
+   </body>
+</html>
