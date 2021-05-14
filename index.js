6965df3fd5f866f358aef2a834019a282661a147
GamesThings <74464331+gamesthings@users.noreply.github.com>
2021-05-14 15:13:41 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+<a class="weatherwidget-io" href="https://forecast7.com/en/28d54n81d38/orlando/" data-label_1="ORLANDO" data-label_2="WEATHER" data-font="Trebuchet MS" data-theme="dark" >ORLANDO WEATHER</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
